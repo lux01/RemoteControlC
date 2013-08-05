88f86dcfc6eb620667cbd92d0f4f2353b8f975fd
Carl Turner <carl@suchideas.com>
2013-08-05 20:54:55 +0100

    Moved resources, added C::B files, tweaked UI

--- RemoteControlC/main.c
+++ RemoteControlC/main.c
@@ -11,13 +11,26 @@
 #include "routes.h"
 #include "mouse.h"
 
+const char* username = "user\0";
+const char* realm = "RemoteControlC\0";
+
+size_t my_gets(char *buff, size_t size){
+    size_t result = 0;
+    if(NULL != fgets(buff, size, stdin)) {
+        char *nlPtr = strchr(buff, '\n');
+        if(nlPtr) *nlPtr = '\0';
+        result = strlen(buff);
+    }
+    return result;
+}
+
 void generate_htpasswd(char* password) {
-	const char* username = "user";
-	const char* realm    = "remote.lux01.co.uk";
 	char *strBuff;
 	char digest[33];
 	int len;
 	FILE *pFile;
+
+	if(strlen(password) == 0) return;
 
 	// Generate the digest to hash
 	len = strlen(username) + strlen(realm) + strlen(password) + 2 + 1; // we need 2 : characters, plus null
@@ -30,15 +43,15 @@ void generate_htpasswd(char* password) {
 
 	// Calculate the digest
 	mg_md5(digest, strBuff, NULL);
-	
+
 	// Clear the undigested string from memory
 	free(strBuff);
 
 	// Write the digest file
-	pFile = fopen(".htpasswd", "w");
+	pFile = fopen("resources/.htpasswd", "w");
 	if(pFile != NULL) {
-		len = strlen(username) + strlen(realm) + 2 + 33 + 1; // 2 for the :, 33 for the md5 hash, 1 for null
-		strBuff = (char*)malloc(len * sizeof(int));
+		len = strlen(username) + strlen(realm) + 2 + 32 + 1; // 2 for the :, 32 for the md5 hash, 1 for null
+		strBuff = (char*)malloc(len * sizeof(char));
 		strcpy(strBuff, username);
 		strcat(strBuff, ":");
 		strcat(strBuff, realm);
@@ -48,7 +61,11 @@ void generate_htpasswd(char* password) {
 		fwrite(strBuff, sizeof(char), len-1, pFile);
 		fwrite("\r\n", sizeof(char), 2, pFile);
 		fclose(pFile);
-		printf(".htpasswd generated successfully! Username is %s\n", username);
+
+		// Clear the digested string from memory
+        free(strBuff);
+
+		printf(".htpasswd generated successfully!\n\nUsername: \t\t%s\nPassword: \t\t%s\n", username, password);
 	} else {
 		printf("Failed to generate .htpasswd!\n");
 		return;
@@ -86,18 +103,19 @@ int main(void) {
 	struct mg_callbacks callbacks;
 	struct WSAData wsaData;
 	char *localIp;
-	char passwdBuff[51];
+	char passwdBuff[52];
 
 	const char *options[] = {
 		"listening_ports", "8080",
 		"num_threads", "1",
-		"authentication_domain", "remote.lux01.co.uk",
+		"authentication_domain", realm,
+		"document_root", "resources/",
 		NULL
 	};
 
 	memset(&callbacks, 0, sizeof(callbacks));
 	callbacks.begin_request = begin_request_handler;
-	
+
 	// Attempt to deduce the local IP
 	if(WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
 		return -1;
@@ -105,18 +123,21 @@ int main(void) {
 	WSACleanup();
 
 	if(localIp != NULL) {
-		printf("Starting remote control server on IP %s ...\n", localIp);
+		printf("Starting remote control server on IP\n"
+          "+------------------+\n"
+          "| %-16s |\n"
+          "+------------------+\n", localIp);
 	}
 
 	// Generate the htpasswd file
-	printf("Please enter a for security (50 characters max.): ");
-	gets_s(passwdBuff, 51);
+	printf("Please enter password (%i characters max., leave blank for none): ",
+        (sizeof passwdBuff)-2); // 2 for \n\0
+	my_gets(passwdBuff, sizeof passwdBuff);
 	generate_htpasswd(passwdBuff);
 
 	// Start Mongoose
 	ctx = mg_start(&callbacks, NULL, options);
-	printf("Listening ports: %s.\n", mg_get_option(ctx, "listening_ports"));
-	
+	printf("Listening ports: \t%s\n\n", mg_get_option(ctx, "listening_ports"));
 
 	printf("Press <ENTER> to terminate.\n");
 
